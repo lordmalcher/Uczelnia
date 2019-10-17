cb23f710206b7f2c24e1f5a3c4e9e60c5dd86322
Bartosz Malcherek <bartoszmalcherek@outlook.com>
2019-10-17 23:17:09 +0200

    KCK lab3 gradients done

--- KCK/lab3/gradients.py
+++ KCK/lab3/gradients.py
@@ -46,75 +46,150 @@ def plot_color_gradients(gradients, names):
 
 
 def hsv2rgb(h, s, v):
-    # TODO
-    return (h, s, v)
+    c = v * s
+    x = c * (1 - abs((h / 60) % 2 - 1))
+    m = v - c
+
+    r = 0
+    g = 0
+    b = 0
+
+    if h >= 0 and h < 60:
+        r = c
+        g = x
+        b = 0
+    if h >= 60 and h < 120:
+        r = x
+        g = c
+        b = 0
+    if h >= 120 and h < 180:
+        r = 0
+        g = c
+        b = x
+    if h >= 180 and h < 240:
+        r = 0
+        g = x
+        b = c
+    if h >= 240 and h < 300:
+        r = x
+        g = 0
+        b = c
+    if h >= 300 and h < 360:
+        r = c
+        g = 0
+        b = x     
+
+    r, g, b = (r + m, g + m, b + m)
+
+    return (r, g, b)
 
 
 def gradient_rgb_bw(v):
     return (v, v, v)
 
+RED_GBR = np.concatenate((
+    np.linspace(0, 0, STEPS/2),
+    np.linspace(0, 1, STEPS/2)
+    ))
+GREEN_GBR = np.concatenate((
+    np.linspace(1, 0, STEPS/2),
+    np.linspace(0, 0, STEPS/2)
+    ))
+BLUE_GBR = np.concatenate((
+    np.linspace(0, 0, STEPS/4),
+    np.linspace(0, 1, STEPS/4),
+    np.linspace(1, 0, STEPS/4),
+    np.linspace(0, 0, STEPS/4)
+    ))
 
 def gradient_rgb_gbr(v):
     step = int(v * STEPS) - 1
 
-    green = np.concatenate((np.linspace(1, 0, STEPS/2), np.linspace(0, 0, STEPS/2)))
-    blue = np.concatenate((np.linspace(0, 0, STEPS/4), np.linspace(0, 1, STEPS/4), np.linspace(1, 0, STEPS/4), np.linspace(0, 0, STEPS/4)))
-    red = np.concatenate((np.linspace(0, 0, STEPS/2), np.linspace(0, 1, STEPS/2)))
+    return (RED_GBR[step], GREEN_GBR[step], BLUE_GBR[step])
 
-    return (red[step], green[step], blue[step])
-
+RED_GRB_FULL = np.concatenate((
+    np.linspace(0, 0, STEPS/2),
+    np.linspace(0, 1, STEPS/4),
+    np.linspace(1, 1, STEPS/4)
+))
+GREEN_GRB_FULL = np.concatenate((
+    np.linspace(1, 1, STEPS/4),
+    np.linspace(1, 0, STEPS/4),
+    np.linspace(0, 0, STEPS/2)
+))
+BLUE_GRB_FULL =np.concatenate((
+    np.linspace(0, 1, STEPS/4),
+    np.linspace(1, 1, STEPS/2),
+    np.linspace(1, 0, STEPS/4)
+))
 
 def gradient_rgb_gbr_full(v):
     step = int(v * STEPS) - 1
 
-    # red = np.linspace(0, 1, STEPS)
-    red = np.concatenate((
-        np.linspace(0, 0, STEPS/2),
-        np.linspace(0, 1, STEPS/4),
-        np.linspace(1, 1, STEPS/4)
-    ))
+    return (RED_GRB_FULL[step], GREEN_GRB_FULL[step], BLUE_GRB_FULL[step])
 
-    green = np.concatenate((
-        np.linspace(1, 1, STEPS/4),
-        np.linspace(1, 0, STEPS/4),
-        np.linspace(0, 0, STEPS/2)
-    ))
-    # green = np.linspace(1, 0, STEPS)
-    # blue = np.concatenate((np.linspace(0, 1, STEPS/2), np.linspace(1, 0, STEPS/2)))
-    blue = np.concatenate((
-        np.linspace(0, 1, STEPS/4),
-        np.linspace(1, 1, STEPS/2),
-        np.linspace(1, 0, STEPS/4)
-    ))
-
-    print(step, red[step], green[step], blue[step])
-
-    return (red[step], green[step], blue[step])
-
+RED_WB_CUSTOM = np.concatenate((
+    np.linspace(1, 1, STEPS/4),
+    np.linspace(1, 0, STEPS/8),
+    np.linspace(0, 0, STEPS/4),
+    np.linspace(0, 1, STEPS/8),
+    np.linspace(1, 1, STEPS/8),
+    np.linspace(1, 0, STEPS/8)
+))
+GREEN_WB_CUSTOM = np.concatenate((
+    np.linspace(1, 0, STEPS/8),
+    np.linspace(0, 0, STEPS/4),
+    np.linspace(0, 1, STEPS/8),
+    np.linspace(1, 1, STEPS/4), 
+    np.linspace(1, 0, STEPS/8), 
+    np.linspace(0, 0, STEPS/8),      
+))
+BLUE_WB_CUSTOM = np.concatenate((
+    np.linspace(1, 1, STEPS/2),
+    np.linspace(1, 0, STEPS/8),
+    np.linspace(0, 0, 3*STEPS/8),
+))
 
 def gradient_rgb_wb_custom(v):
-    # TODO
-    return (0, 0, 0)
+    step = int(v * STEPS) - 1
 
+    return (RED_WB_CUSTOM[step], GREEN_WB_CUSTOM[step], BLUE_WB_CUSTOM[step])
+
+V_BW = np.linspace(0, 1, STEPS)
 
 def gradient_hsv_bw(v):
-    # TODO
-    return hsv2rgb(0, 0, 0)
+    step = int(v * STEPS) - 1
 
+    return hsv2rgb(0, 0, V_BW[step])
+
+H_GBR = np.linspace(120, 360, STEPS)
 
 def gradient_hsv_gbr(v):
-    # TODO
-    return hsv2rgb(0, 0, 0)
+    step = int(v * STEPS) - 1
 
+    s = 1
+    v = 1
+
+    return hsv2rgb(H_GBR[step], s, v)
+
+H_UNKNOWN = np.linspace(120, 0, STEPS)
 
 def gradient_hsv_unknown(v):
-    # TODO
-    return hsv2rgb(0, 0, 0)
+    step = int(v * STEPS) - 1
 
+    s = 0.5
+    v = 1
+
+    return hsv2rgb(H_UNKNOWN[step], s, v)
+
+H_CUSTOM = np.linspace(0, 360, STEPS)
+S_CUSTOM = np.linspace(0.25, 0.75, STEPS)
+V_CUSTOM = np.linspace(0.5, 1, STEPS)
 
 def gradient_hsv_custom(v):
-    # TODO
-    return hsv2rgb(0, 0, 0)
+    step = int(v * STEPS) - 1
+
+    return hsv2rgb(H_CUSTOM[step], S_CUSTOM[step], V_CUSTOM[step])
 
 
 if __name__ == '__main__':
